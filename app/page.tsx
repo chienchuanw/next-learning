ba3ff0832e8ff117d452161dc0fd85a1f6604676
chienchuanw <chienchuanwww@gmail.com>
2024-09-11 10:45:31 +0800

    Finished Routing and Navigation

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,17 @@
 import Image from 'next/image'
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main><h1>Hello World</h1></main>
+    <main>
+      <h1>Hello World</h1>
+      {/* Old way using a tag which will reload whole page when click the link*/}
+
+      {/* <a href="/users">Users</a> */}
+
+      {/* Using Link tag instead for optimizing the loading */}
+      {/* Remember to import Link first before using the Link tag */}
+      <Link href="/users">Users</Link>
+    </main>
   )
 }
