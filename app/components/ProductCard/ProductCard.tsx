306a7ce5ed251006d8ea9ccf52e22e8fd13effe5
chienchuanw <chienchuanwww@gmail.com>
2024-09-11 15:46:03 +0800

    Finished Tailwind CSS

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import AddToCart from '../AddToCart'
 
 // The name of "styles" can be anything, but it's recommended to use styles for recognition
+// Because we use Tailwindcss instead of CSS module, so this line is now unused.
 import styles from './ProductCard.module.css'
 
 const ProductCard = () => {
   return (
-    <div className={styles.card}>
+    <div className='p-5 my-5 bg-sky-400 text-white text-xl hover:bg-sky-500'>
       <AddToCart />
     </div>
   )
